19ec96e8d110b1970b4b56029e6fe0b8134ff4d4
yuva-durga <yuvadurga13@gmail.com>
2025-12-17 19:35:56 +0530

    Reusable MessageCard with props

--- react-assignment/src/App.jsx
+++ react-assignment/src/App.jsx
@@ -1,14 +1,19 @@
-import Calculator from "./calculator/Calculator";
+import MessageCard from "./messagecard/MessageCard";
 
 function App() {
   return (
     <div>
-      <h2> Calculator</h2>
-      <Calculator />
+      <h2>Message Cards</h2>
+
+      <MessageCard title="Hello" message="Welcome to React" />
+      <MessageCard title="Info" message="Props are reusable" />
+      <MessageCard title="Note" message="Each card is different" />
     </div>
   );
 }
 
 export default App;
+
+
 
 
